9cadce7e27cd8075e5097658f7c85e3fb8a9cdc4
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-29 17:45:16 +0200

    complete  random name to elephant in PL lang

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 const btn = document.querySelector(".roll");
 const dev = document.querySelector(".name");
 
-const names = ["Słoniczyk", "Sloniczyszek", "Słunik", "Słoneczko", "Słoniczynka"];
+const names = ["SŁONICZYK", "SŁONISZEK", "SŁUNIK", "SŁONIACZKO", "SŁONICZYNKA"];
 
 
 const showName = () => {
     const index = Math.floor(Math.random() * 5);
     dev.textContent = `This name is ${names[index]}`;
 }
-
-
-
 
 btn.addEventListener("click", showName)
