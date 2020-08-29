6eea0438416457b3eb4e1aad14be842c6e12dea9
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-29 17:52:30 +0200

    better option to random with prefix

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const btn = document.querySelector(".roll");
 const dev = document.querySelector(".name");
 
 const names = ["SŁONICZYK", "SŁONISZEK", "SŁUNIK", "SŁONIACZKO", "SŁONICZYNKA"];
+const prefixs = ["Im sure that", "I think", "In my opinion", "I wish"]
 
 
 const showName = () => {
-    const index = Math.floor(Math.random() * 5);
-    dev.textContent = `This name is ${names[index]}`;
+    const index = Math.floor(Math.random() * names.length);
+    const prefix = Math.floor(Math.random() * prefixs.length);
+    dev.textContent = ` ${prefixs[prefix]}, this name is ${names[index]}`;
 }
 
 btn.addEventListener("click", showName)
