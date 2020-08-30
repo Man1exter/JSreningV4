589090be44e0aef77827efaaa373452318d9ecd4
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-30 19:50:47 +0200

    full complete exercises

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector(".roll");
 const dev = document.querySelector(".name");
 
-const names = ["SŁONICZYK", "SŁONISZEK", "SŁUNIK", "SŁONIACZKO", "SŁONICZYNKA"];
+const names = ["SŁONICZYK", "SŁONISZEK", "SŁUNIK", "SŁONIACZKO", "SŁONICZYNKA","SŁOWIANECZKO"];
 const prefixs = ["Im sure that", "I think", "In my opinion", "I wish"]
 
 
@@ -18,27 +18,29 @@ btn.addEventListener("click", showName)
 ////RANDOM CODE WITH 10 CHArS :))
 //-----//------------------------------------------------
 
-const chars = 'ABCDEFG0123456789';
+const chars = 'ABCDEFGHIJ0123456789';
 
 const clicks = document.querySelector(".click");
 const section = document.querySelector("section");
 
-const chartsWidth = 10;
-const codeNumbers  = 10;
+const codeNumbers  = 10; //ilosc numerow
+const chartsWidth = 10; //dlugosc numerow
 
-const functionMove = () => {
- for(let i=o; i < codeNumbers; i++) {
+
+const functionMoves = () => {
+ for(let i = 0; i < codeNumbers; i++) {
        let code = '';
            for(let i = 0; i < chartsWidth; i++) {
                const index = Math.floor(  Math.random()  *  20);
-               code += charts[index];
+               code += chars[index];
            }
           const div = document.createElement('div');
                   div.textContent = code;
                       section.appendChild(div);
+                      
         }
 }
 
-clicks.addEventListener("click", functionMove)
+clicks.addEventListener("click", functionMoves );
 
 
